3458ff896dceda0c25fcac1672161c998e43d30f
Karlivar21 <karlk21@ru.is>
2024-08-08 22:11:06 +0000

    day

--- orders-frontend/src/components/Day/index.jsx
+++ orders-frontend/src/components/Day/index.jsx
@@ -1,9 +1,8 @@
-// DayView.js
 import React from 'react';
-import './styles.css'
 
 const DayView = ({ orders, date }) => {
     const dayOrders = orders.filter(order => new Date(order.date).toLocaleDateString('is-IS') === date);
+
     const formatProducts = (order) => {
         const products = [];
 
@@ -23,27 +22,27 @@ const DayView = ({ orders, date }) => {
     };
 
     return (
-        <div className="day-view">
-            <h2>{date}</h2>
+        <div className="flex flex-col items-center p-6">
+            <h2 className="text-2xl font-bold mb-4">{date}</h2>
             {dayOrders.length > 0 ? (
-                <ul>
+                <div className="flex flex-col w-full max-w-4xl border-2 border-blue-700 rounded-lg p-4 space-y-4 bg-white shadow-lg">
                     {dayOrders.map((order, index) => (
-                        <div className="day-column1" key={index}>
-                            <div className='day-details'>
-                                {order.name}-
-                                {order.phone}- 
-                                {order.email}
+                        <div className="flex flex-col md:flex-row border border-blue-700 rounded-lg p-4 space-y-4 md:space-y-0 md:space-x-4 bg-gray-100" key={index}>
+                            <div className="flex-1 bg-gray-200 p-4 rounded-lg">
+                                <p className="font-semibold text-lg">{order.name}</p>
+                                <p>{order.phone}</p>
+                                <p>{order.email}</p>
                             </div>
-                            <div className='day-products'>
-                            {formatProducts(order).map((product, index) => (
-                                        <div key={index}>{product}</div>
-                                    ))}
+                            <div className="flex-1 bg-gray-200 p-4 rounded-lg">
+                                {formatProducts(order).map((product, index) => (
+                                    <p key={index} className="text-sm">{product}</p>
+                                ))}
                             </div>
                         </div>
                     ))}
-                </ul>
+                </div>
             ) : (
-                <p>No orders</p>
+                <p className="text-gray-500">No orders</p>
             )}
         </div>
     );
